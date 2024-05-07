87bac12c38db2d284f10668f22e731dce87344dd
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-05-07 11:32:45 +0300

    Update 1.ts

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// Не розумію чому повертаємо T, якщо Т це рядок. А в умові завдання сказано, що повертати потрібно об'єкт?
 export async function fetchData<T extends string>(url: T): Promise<T> {
   try {
     const response = await axios.get(url);
